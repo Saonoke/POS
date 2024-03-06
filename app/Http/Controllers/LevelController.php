cede58e87b64391c857397598e5c51e50bf9f631
krisna <saonoke21@gmail.com>
2024-03-06 20:31:54 +0700

    Feat : DB Facade

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class LevelController extends Controller
+{
+    // //
+    // public function index()
+    // {
+    //     DB::insert('insert into m_level(level_kode,level_nama,created_at) values(?,?,?)', ['CUS', 'Pelanggan', now()]);
+
+    //     return 'Insert data baru berhasil';
+    // }
+    // public function index()
+    // {
+    //     DB::update('update m_level set level_nama=? where level_kode = ?', ['Customer', 'CUS']);
+
+    //     return 'Update data baru berhasil';
+    // }
+
+    // public function index()
+    // {
+    //     $row = DB::delete('delete from m_level where level_kode = ?', ['cus']);
+
+    //     return 'Delete data berhasil. jumlah data yang dihapus ' . $row . ' baris';
+    // }
+
+    public function index()
+    {
+        $data = DB::select('select * from m_level');
+
+        return view('level.level', ['data' => $data]);
+    }
+
+
+
+}
--- database/migrations/2024_03_06_073423_create_m_barang_table.php
+++ database/migrations/2024_03_06_073423_create_m_barang_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('m_barang', function (Blueprint $table) {
             $table->id('barang_id');
-            $table->unsignedBigInteger('kategori_id')->index();
+            $table->string('kategori_id')->index();
             $table->string('barang_kode', 10)->unique();
             $table->string('barang_nama', 100);
             $table->integer('harga_beli');
